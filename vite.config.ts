c399ded071ffc495abc1056a7c747ec37c2e0d1c
Evandro Calado <evandrocalado07@gmail.com>
2024-04-11 12:57:14 -0300

    🐞 fix: lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,7 +20,7 @@ export default defineConfig({
     },
   },
   define: {
-    "process.env": process.env,
-    global: "window",
+    'process.env': process.env,
+    global: 'window',
   },
 });
